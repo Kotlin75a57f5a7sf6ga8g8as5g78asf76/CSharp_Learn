07936c19218ac7885ec3ff4cdb1ea4c6e93321f0
Зуев Александр <neoapas777veadvx310000@gmail.com>
2020-11-03 13:20:09 +0300

    Выполнил задачу 3

---
+++ TrainingPractice_01/ZAV_Task_03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ZAV_Task_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Пароль
+            const string password = "12345";
+
+            string userPassword = null;
+            int mistakes = 0;
+
+            // Цикл до 3 ошибок
+            while (mistakes != 3)
+            {
+                Console.WriteLine($"Введите пароль, осталось попыток {3 - mistakes}.");
+                userPassword = Convert.ToString(Console.ReadLine());
+
+                if (userPassword == password)
+                {
+                    Console.WriteLine("Cекретное сообщение =)");
+                    break;
+                }
+                mistakes++;
+            }
+        }
+    }
+}
