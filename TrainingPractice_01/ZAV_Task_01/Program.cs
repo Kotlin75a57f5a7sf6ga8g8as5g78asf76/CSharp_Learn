9f193501d2e3f5f3ca8e148c169944ef5ab2ac07
Зуев Александр <neoapas777veadvx310000@gmail.com>
2020-11-03 12:02:48 +0300

    Выполнил задачу 1

--- TrainingPractice_01/ZAV_Task_01/Program.cs
+++ TrainingPractice_01/ZAV_Task_01/Program.cs
@@ -6,7 +6,38 @@ namespace ZAV_Task_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Стоимость 1 кристалла
+            const int price = 100;
+
+            Console.WriteLine("Введите колличество золота:");
+            int gold = Convert.ToInt32(Console.ReadLine());
+
+            // Проверяем возможна ли покупка в магазине
+            while (gold < price)
+            {
+                Console.WriteLine($"Вы не можете зайти в магазин пока ваще золото не будет больше {price} иначе вы ничего не сможете купить.");
+                Console.WriteLine("Введите колличество золота:");
+                gold = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
+            int crystal = Convert.ToInt32(Console.ReadLine());
+
+            bool flag = Convert.ToBoolean(gold / (crystal * price));
+            
+            // Используем цикл while вместо if else.
+            while (!flag)
+            {
+                Console.WriteLine($"Невозможно совершить покупку, так как у вас {gold} золота, а вы хотите купить {crystal} кристалов."); ;
+                Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
+                crystal = Convert.ToInt32(Console.ReadLine());
+
+                flag = Convert.ToBoolean(gold / (crystal * price));
+            }
+
+            gold = gold - crystal * price;
+
+            Console.WriteLine($"Сделка удалась. У вас {gold} золота и {crystal} кристалов");
         }
     }
 }
