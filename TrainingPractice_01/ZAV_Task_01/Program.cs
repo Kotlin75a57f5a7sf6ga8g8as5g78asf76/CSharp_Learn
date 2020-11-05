c60354b2f83b64030b0f5d4e2adbec4a64a55048
Зуев Александр <neoapas777veadvx310000@gmail.com>
2020-11-05 10:24:08 +0300

    Исправил задачу 1

--- TrainingPractice_01/ZAV_Task_01/Program.cs
+++ TrainingPractice_01/ZAV_Task_01/Program.cs
@@ -8,36 +8,52 @@ namespace ZAV_Task_01
         {
             // Стоимость 1 кристалла
             const int price = 100;
+            int crystal = 0;
 
             Console.WriteLine("Введите колличество золота:");
             int gold = Convert.ToInt32(Console.ReadLine());
 
-            // Проверяем возможна ли покупка в магазине
-            while (gold < price)
-            {
-                Console.WriteLine($"Вы не можете зайти в магазин пока ваще золото не будет больше {price} иначе вы ничего не сможете купить.");
-                Console.WriteLine("Введите колличество золота:");
-                gold = Convert.ToInt32(Console.ReadLine());
-            }
-
             Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
-            int crystal = Convert.ToInt32(Console.ReadLine());
+            crystal = Convert.ToInt32(Console.ReadLine());
 
-            bool flag = Convert.ToBoolean(gold / (crystal * price));
-            
-            // Используем цикл while вместо if else.
-            while (!flag)
+            if (gold < crystal * price)
             {
-                Console.WriteLine($"Невозможно совершить покупку, так как у вас {gold} золота, а вы хотите купить {crystal} кристалов."); ;
-                Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
-                crystal = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine($"Невозможно совершить покупку, так как у вас {gold} золота, а вы хотите купить {crystal} кристалов.");
 
-                flag = Convert.ToBoolean(gold / (crystal * price));
+            }
+            else
+            {
+                gold = gold - crystal * price;
+                Console.WriteLine($"Сделка удалась. У вас {gold} золота и {crystal} кристалов");
             }
 
-            gold = gold - crystal * price;
-
-            Console.WriteLine($"Сделка удалась. У вас {gold} золота и {crystal} кристалов");
         }
     }
 }
+
+// Используем цикл while вместо if else.
+/*while (!flag)
+{
+    Console.WriteLine($"Невозможно совершить покупку, так как у вас {gold} золота, а вы хотите купить {crystal} кристалов."); ;
+    Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
+    crystal = Convert.ToInt32(Console.ReadLine());
+
+    flag = Convert.ToBoolean(gold / (crystal * price));
+}*/
+
+
+/*if (gold >= price)
+{
+    Console.WriteLine($"Сколько кристаллов вы хотите купить? Курс 1 кр. = {price} золота");
+    crystal = Convert.ToInt32(Console.ReadLine());
+    if (gold < crystal * price)
+    {
+        Console.WriteLine($"Невозможно совершить покупку, так как у вас {gold} золота, а вы хотите купить {crystal} кристалов.");
+
+    }
+    bool flag = Convert.ToBoolean(gold / (crystal * price));
+    gold = gold - crystal * price;
+}
+else
+    Console.WriteLine($"У вас недостаточно золота чтобы купить кристаллы");
+Console.WriteLine($"Сделка удалась. У вас {gold} золота и {crystal} кристалов");*/
--- TrainingPractice_01/ZAV_Task_04/Program.cs
+++ TrainingPractice_01/ZAV_Task_04/Program.cs
@@ -9,8 +9,15 @@ namespace ZAV_Task_04
             Random random = new Random();
             int typeOfBoss = random.Next(0, 10);
             int playerHealth = random.Next(700, 1000);
+            int playerHealthFull = playerHealth;
             int bossHealth = random.Next(900, 1300);
             bool turn = Convert.ToBoolean(random.Next(0, 2));
+            string playerEffects = "Нет";
+            int playerAttack = 0;
+            int damage = 0;
+            int abyssAttackUses = 0;
+            int playerHealing = 0;
+            int theLastContractUses = 0;
 
             string typeOfBossString = null;
             switch (typeOfBoss)
@@ -53,18 +60,92 @@ namespace ZAV_Task_04
             else
                 Console.WriteLine($"На вас напал(а) {typeOfBossString}");
 
-            Console.WriteLine($"Здоровье игрока: {playerHealth}");
-            Console.WriteLine($"Здоровье босса:  {bossHealth}");
+            while (playerHealth > 0 && bossHealth > 0)
+            {
+                Console.WriteLine($"Здоровье игрока: {playerHealth}");
+                Console.WriteLine($"Здоровье босса:  {bossHealth}");
+                Console.WriteLine($"Эффекты:         {playerEffects}");
 
-            Console.WriteLine("Выберите атуку (Чтобы выбрать введите номер атаки):");
-            Console.WriteLine("1) Атака посохом (Атака: 50-100) - Слабая атака посохом.");
-            Console.WriteLine("2) Призыв армии мёртвых (Урон по себе: 250, Атака: 10-25, Эффект: Армия мёртвых) " +
-                              "- Это заклинание лучше использывать с чём то другим.");
-            Console.WriteLine("3) Атака Бездны (Условие: Армия мёртвых, Атака 100-200) - Бездна атакует врага.");
-            Console.WriteLine("4) Последний контракт (Условие: Ваш уровень здоровья должен быть ниже 25%, " +
-                "Восстановление здоровья: 250 - 500, Недостаток: Можно использывать один раз за бой)");
-            Console.WriteLine("5) ");
-            Console.ReadLine();
+                Console.WriteLine("Выберите атуку (Чтобы выбрать введите номер атаки):");
+                Console.WriteLine("1) Атака посохом (Атака: 50-100) - Слабая атака посохом.");
+                Console.WriteLine("2) Призыв армии мёртвых (Урон по себе: 250, Атака: 10-25, Эффект: Армия мёртвых) " +
+                                  "- Это заклинание лучше использывать с чём то другим.");
+                Console.WriteLine("3) Атака Бездны (Условие: Армия мёртвых, Атака 100-200) - Бездна атакует врага.");
+                Console.WriteLine("4) Последний контракт (Условие: Ваш уровень здоровья должен быть ниже 25%, " +
+                                  "Восстановление здоровья: 250 - 500, Недостаток: Можно использывать один раз за бой)");
+                Console.WriteLine("5) Армагеддон (Урон по себе: 25%, Атака 10%) - Вы вызываете армагеддон.");
+                Console.WriteLine("0) Пропустить ход.");
+                Console.WriteLine("Ваш выбор:");
+                playerAttack = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("");
+                switch (playerAttack)
+                {
+                    case 0:
+                        Console.WriteLine("0) Вы пропустили ход.");
+                        Console.WriteLine();
+                        break;
+                    case 1:
+                        Console.WriteLine($"1) Вы атаковали {typeOfBossString} на {damage = random.Next(50, 101)}");
+                        Console.WriteLine();
+                        bossHealth -= damage;
+                        break;
+                    case 2:
+                        Console.WriteLine($"2) Вы призвали армию мёртвых и атаковали {typeOfBossString} на {damage = random.Next(10, 26)}" +
+                                $"Вы потеряли 250 здоровья.");
+                        Console.WriteLine();
+                        playerEffects = "Армия мёртвых";
+                        playerHealth -= 250;
+                        bossHealth -= damage;
+                        break;
+                    case 3:
+                        if (playerEffects == "Армия мёртвых")
+                        {
+                            abyssAttackUses++;
+                            Console.WriteLine($"3) Вы успешно использовали Атаку Бездны и атаковали {typeOfBossString} на " +
+                            $"{damage = random.Next(100, 201)}");
+                            bossHealth -= damage;
+                            if (abyssAttackUses == 2)
+                            {
+                                playerEffects = "Нет";
+                                Console.WriteLine($"3) Эффект Армия мёртвых закончился.");
+                            }
+                            Console.WriteLine();
+                        }
+                        else 
+                            Console.WriteLine($"3) Условие использования Атаки Бездны не соблюдено, вы пропускаете ход");
+                        Console.WriteLine();
+                        break;
+                    case 4:
+                        if (playerHealth < (playerHealthFull / 100) * 25 && theLastContractUses == 0)
+                        {
+                            theLastContractUses++;
+                            Console.WriteLine($"4) Вы успешно использовали Последний контракт и восстановили себе" +
+                                $" {playerHealing = random.Next(250, 501)} здоровья");
+                            playerHealth += playerHealing;
+                        }
+                        else
+                            Console.WriteLine($"4) Условие использования Последнего контракта не соблюдено, вы пропускаете ход");
+                        Console.WriteLine();
+                        break;
+                    case 5:
+                        Console.WriteLine($"5) Вы использовали Армагеддон. (Урон по себе: 25%, Атака 10%).");
+                        playerHealthFull -= (playerHealthFull / 100) * 25;
+                        bossHealth -= (bossHealth / 100) * 25;
+                        break;
+                    case 6:
+                        typeOfBossString = "Элементаль Магмы";
+                        break;
+                    case 7:
+                        typeOfBossString = "Могучая Горгона";
+                        break;
+                    case 8:
+                        typeOfBossString = "Великий Василиск";
+                        break;
+                    case 9:
+                        typeOfBossString = "Гидра Хаоса";
+                        break;
+                }
+            }
         }
     }
 }
