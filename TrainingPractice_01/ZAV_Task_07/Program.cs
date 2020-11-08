c818288526a70f42fa1eef93e9556a4c8e407c8c
Зуев Александр <neoapas777veadvx310000@gmail.com>
2020-11-08 14:28:33 +0300

    Выполнил задачу 7

---
+++ TrainingPractice_01/ZAV_Task_07/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ZAV_Task_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            Console.WriteLine("Введите размер массива: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 100);
+            }
+
+            Console.WriteLine("\nИсходный массив:");
+            PrintArray(array);
+
+            Shuffle(array);
+            Console.WriteLine("\nПеремешанный массив:");
+            PrintArray(array);
+        }
+        static void PrintArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.Write($"{array[i]} ");
+            Console.Write("\n");
+        }
+        static void Shuffle(int[] arr)
+        {
+            Random random = new Random();
+
+            for (int i = arr.Length - 1; i >= 1; i--)
+            {
+                int j = random.Next(i + 1);
+
+                int tmp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = tmp;
+            }
+        }
+    }
+}
